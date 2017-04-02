8e521869bfd8665782e24561e0b5fcd12d9e11db
Goh Qi liang <qliang91@gmail.com>
2017-04-02 15:44:29 +0800

    Changed mongodb path

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://qliang:12345@ds041526.mlab.com:41526/meanauthapp',
+    database: 'mongodb://qliang91:12345@ds041526.mlab.com:41526/meanauthapp',
     //database: 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecrete'
 }
